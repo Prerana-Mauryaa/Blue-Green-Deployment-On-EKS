98b3ce8c7dfe49c75496aa73e9ea999d1c765095
Prerana Maurya <preranamaurya012389@gmail.com>
2024-12-13 00:03:03 +0530

    push

--- helm-charts/switch-traffic.sh
+++ helm-charts/switch-traffic.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Set namespace and ingress name
 INGRESS_NAME=$1
+CHART_PATH=$2
+HELM_RELEASE_NAME=$3  # Helm release name
 
-# Define services
+# Define service names and ports
 SERVICE_1_NAME="service-flaskapp-prod"
-SERVICE_1_PORT=80
 SERVICE_2_NAME="service-flaskapp-preprod"
-SERVICE_2_PORT=80
 
-# Get current service being used in the ingress
+# Get the current service being used in the ingress
 CURRENT_SERVICE=$(kubectl get ingress $INGRESS_NAME -o=jsonpath='{.spec.rules[0].http.paths[0].backend.service.name}')
 
-# Check which service is currently in use and switch
+# Check which service is currently in use and switch traffic
 if [ "$CURRENT_SERVICE" == "$SERVICE_1_NAME" ]; then
   echo "Switching traffic from $SERVICE_1_NAME to $SERVICE_2_NAME"
-  kubectl patch ingress $INGRESS_NAME  \
-    -p "{\"spec\":{\"rules\":[{\"host\":\"your-prod-host\",\"http\":{\"paths\":[{\"path\":\"/\",\"pathType\":\"Prefix\",\"backend\":{\"service\":{\"name\":\"$SERVICE_2_NAME\",\"port\":{\"number\":$SERVICE_2_PORT}}}}]}}]}}"
+
+  # Upgrade Helm chart to switch to the pre-prod service
+  helm upgrade $HELM_RELEASE_NAME ${CHART_PATH} \
+    --set ingress.services.prod.name=$SERVICE_2_NAME \
+    --set ingress.services.qaPreprod.name=$SERVICE_2_NAME \
+
 else
   echo "Switching traffic from $SERVICE_2_NAME to $SERVICE_1_NAME"
-  kubectl patch ingress $INGRESS_NAME  \
-    -p "{\"spec\":{\"rules\":[{\"host\":\"your-prod-host\",\"http\":{\"paths\":[{\"path\":\"/\",\"pathType\":\"Prefix\",\"backend\":{\"service\":{\"name\":\"$SERVICE_1_NAME\",\"port\":{\"number\":$SERVICE_1_PORT}}}}]}}]}}"
+
+  # Upgrade Helm chart to switch to the prod service
+  helm upgrade $HELM_RELEASE_NAME ${CHART_PATH} \
+    --set ingress.services.prod.name=$SERVICE_1_NAME \
+    --set ingress.services.qaPreprod.name=$SERVICE_1_NAME \
 fi
 
 echo "Traffic switch complete."
